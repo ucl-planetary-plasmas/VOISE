55bfc9de7942c382d5c9e4bfa35c438d43cd406c
patrick <patrick>
2009-03-20 17:12:51 +0000

    *** empty log message ***

--- matlab/computeVDFast.m
+++ matlab/computeVDFast.m
@@ -1,8 +1,8 @@
-function VD = computeVD1(nr, nc, S)
-% function VD = computeVD1(nr, nc, S)
+function VD = computeVDFast(nr, nc, S)
+% function VD = computeVDFast(nr, nc, S)
 
 %
-% $Id: computeVDFast.m,v 1.1 2009/02/08 21:07:18 patrick Exp $
+% $Id: computeVDFast.m,v 1.2 2009/03/20 17:12:51 patrick Exp $
 %
 % Copyright (c) 2008 
 % Patrick Guio <p.guio@ucl.ac.uk>
@@ -20,18 +20,99 @@ function VD = computeVD1(nr, nc, S)
 % Public License for more details.
 %
 
+% update VD structure
+VD.nr = nr;
+VD.nc = nc;
 
-VD = initVD(nr, nc, S);
+% set limits of W
+VD.xm = 1;
+VD.ym = 1;
+VD.xM = nc;
+VD.yM = nr;
 
+% initialise grid for image points coordinates
+xi = 1:nc;
+yi = 1:nr;
+
+% (VD.x(i,j), VD.y(i,j)) contains the coordinates of the point
+% with indices (i,j) in array image data
+[VD.x, VD.y] = meshgrid(xi,yi);
+
+% check whether all seeds are in W
+inW = isXinW(S, VD);
+if ~isempty(find(~inW)),
+  s = sprintf('Error: seeds outside the image limits\n');
+  ii = find(~inW);
+  s = [s sprintf('\tk=%3d (%3d,%3d)\n', [ii, S(ii,1:2)]')];
+  s = [s sprintf('\n')];
+  error(s);
+end
+
+% seed's index and coordinates 
 ns = size(S, 1);
-for k = 3:ns,
-  VD = addSeedToVDFast(VD, S(k,:));
-  drawVD(VD);
-  if 0,
-    fprintf(1,'.');
+VD.Sk = [1:ns]';
+VD.Sx = S(VD.Sk,1);
+VD.Sy = S(VD.Sk,2);
+
+% Calculate Voronoi Diagram for current seeds
+[V, C] = voronoin([VD.Sx(VD.Sk), VD.Sy(VD.Sk)]);
+
+% allocate cells for seed's neighbour list`
+Nk = cell(ns, 1);
+for s = 1:ns, % for each seed s
+  %fprintf(1,'seed %d:\n', s);
+  for i = 1:length(C{s}), % for all vertices in VR(s)
+    iv = C{s}(i);
+    %fprintf(1,' iv=%d, (%f,%f)\n', iv,  V(iv,1:2));
+    if iv ~= 1, % not infinite vertex
+      for r = setdiff([1:ns], s), % for all seeds S\s
+        if ~isempty(find(C{r}(:) == iv)),
+					Nk{s} = [Nk{s}; setdiff(r, Nk{s})];
+          %si = VD.Sk(s);
+          %ri = VD.Sk(r);
+          %Nk{si} = [Nk{si}; setdiff(ri, Nk{si})];
+          %Nk{ri} = [Nk{ri}; setdiff(si, Nk{ri})];
+        end
+      end
+    end
   end
 end
-fprintf(1,'\n');
+
+VD.Nk = Nk;
+
+% allocate and initialise (lambda,v) structure 
+% all points p in W are in VR(1)
+VD.Vk.lambda = ones(nr,nc);
+% all points have only one nearest seed
+VD.Vk.v = zeros(nr,nc);
+
+for s = 2:ns, % for each seed s
+  [ii, jj, ij] = getVRclosure(VD, s, VD.Nk{s});
+
+	% compute distance functions
+	% \mu = d^2(V_1[i,j].\lambda, p), p=(j,i) in W
+	mu  = (VD.x(ij)-VD.Sx(VD.Vk.lambda(ij))).^2+...
+	      (VD.y(ij)-VD.Sy(VD.Vk.lambda(ij))).^2;
+	% \mu = d^2(s*=2, p), p=(j,i) in W
+	mus = (VD.x(ij)-VD.Sx(s)).^2        +(VD.y(ij)-VD.Sy(s)).^2;
+	% case mu* > mu in Eq. 3.1
+	% V_2[i,j] = V_1[i,j]
+	Vk = VD.Vk;
+	% case mu* < mu in Eq. 3.1
+	% V_2[i,j] = (s*=2, 0)
+	iless = find(mus < mu);
+	Vk.lambda(ij(iless)) = s;
+	Vk.v(ij(iless)) = 0;
+	% case mu* == mu in Eq. 3.1
+	% V_2[i,j] = (\lambda, 1)
+	iequal = find(mus == mu);
+	Vk.v(ij(iequal)) = 1;
+
+  % Update Vk 
+	VD.Vk = Vk;
+
+end
+
 
 fprintf(1,'Voronoi Diagram computed\n')
 %pause
