0e784fcdc175f171d45483fd9c550df00cc7bbd4
patrick <patrick>
2009-07-06 14:07:42 +0000

    Completed and tested code.

--- matlab/getVRMaxAngle.m
+++ matlab/getVRMaxAngle.m
@@ -1,8 +1,8 @@
-function L = getVRMaxAngle(VD, sk)
-% function L = getVRMaxAngle(VD, sk)
+function maxAngle = getVRMaxAngle(VD, sk)
+% function maxAngle = getVRMaxAngle(VD, sk)
 
 %
-% $Id: getVRMaxAngle.m,v 1.1 2009/07/04 13:21:54 patrick Exp $
+% $Id: getVRMaxAngle.m,v 1.2 2009/07/06 14:07:42 patrick Exp $
 %
 % Copyright (c) 2009 
 % Patrick Guio <p.guio@ucl.ac.uk>
@@ -20,29 +20,69 @@ function L = getVRMaxAngle(VD, sk)
 % Public License for more details.
 %
 
-% get vertices list of VR(sk)
-[V,I] = getVRvertices(VD, sk);
-% close the neighbour list 
-is = [[1:length(I)],1];
+% get vertices list of VR(sk) 
 
+[V,I] = getVRvertices(VD, sk);
+
+% close list of vertice points 
 x = [V(:,1); V(1,1)];
 y = [V(:,2); V(1,2)];
 
+% list of vertice vectors
 vx = diff(x);
 vy = diff(y);
 
-dot(vx(1:end-1))
+% normalise vectors
+normv = sqrt(vx.^2+vy.^2);
+vx = vx./normv;
+vy = vy./normv;
 
-if all(isfinite(x)) & all(isfinite(y))
-  % sum(sqrt(sum((V([1:end],:)-V([2:end 1],:)).^2,2)))
-  L = sum(sqrt(diff(x).^2+diff(y).^2))
-	if 0
-	plot(x,y,'-x');
-	for i=1:length(x)-1,
+vx = [vx(end); vx];
+vy = [vy(end); vy];
+
+right =  [vx([2:end]), vy([2:end])]';
+left  = -[vx([1:end-1])  , vy([1:end-1])]';
+
+% calculate the cosine of interior angles for all vertices
+cosAB = dot(left,right);
+
+% derive the angle in radian within 0 and \pi
+Angles = acos(cosAB(:));
+
+% maximum angle
+maxAngle = max(Angles);
+
+if 0, % diagnostic plot
+
+if all(isfinite(Angles))
+  numSumAngles = sum(Angles);
+  % the sum of the interior angles of any polygon is \pi*(n_v-2)
+  % where n_v is the number of vertices
+  nv = size(V,1);
+  theSumAngles = pi*(nv-2)
+  fprintf(1,'sum Angles %f (%f) abs(error) %g\n', ...
+          180/pi*[numSumAngles, theSumAngles, abs(numSumAngles-theSumAngles)]);
+end
+
+subplot(211),
+plot(x,y,'-x');
+for i=1:length(x)-1,
+  if isfinite(x(i)) & isfinite(y(i)),
 	  text(x(i), y(i), num2str(i), 'verticalalignment', 'bottom');
 	end
-	pause
+end
+axis equal
+subplot(212)
+for i=1:size(left,2),
+
+  if isfinite(right(:,i)) & isfinite(left(:,i)),
+	  plot([0 left(1,i)],[0 left(2,i)], [0 right(1,i)], [0 right(2,i)])
+	  axis equal
+	  fprintf(1, 'vertice %d cos(Angle) %+.2f Angle %6.1f\n', ...
+	          i, cosAB(i), 180/pi*Angles(i));
+	  pause
 	end
-else
-  L = Inf;
+
+end
+
 end
