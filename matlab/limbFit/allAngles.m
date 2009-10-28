091804cba5e800f3056e6f160fafc43c05146cd9
patrick <patrick>
2009-10-28 14:36:20 +0000

    Default function to select all available angles.

---
+++ matlab/limbFit/allAngles.m
@@ -0,0 +1,24 @@
+function flag = allAngles(T)
+% function flag = allAngles(T)
+
+%
+% $Id: allAngles.m,v 1.1 2009/10/28 14:36:20 patrick Exp $
+%
+% Copyright (c) 2009 
+% Patrick Guio <p.guio@ucl.ac.uk>
+%
+% All Rights Reserved.
+%
+% This program is free software; you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by the
+% Free Software Foundation; either version 2.  of the License, or (at your
+% option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+% Public License for more details.
+%
+
+flag = true(size(T));
+
