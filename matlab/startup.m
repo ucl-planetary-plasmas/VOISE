7f17e7143f3d06dcd7beeeb7b1e3fad57df4a590
patrick <patrick>
2009-11-10 16:34:18 +0000

    Default Matlab startup for VOISE.

---
+++ matlab/startup.m
@@ -0,0 +1,47 @@
+% 
+% start up file for VOISE
+%
+
+%
+% $Id: startup.m,v 1.1 2009/11/10 16:34:18 patrick Exp $
+%
+% Copyright (c) 2009 
+% Patrick Guio <p.guio@ucl.ac.uk>
+%
+% All Rights Reserved.
+%
+% This program is free software; you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by the
+% Free Software Foundation; either version 2.  of the License, or (at your
+% option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+% Public License for more details.
+%
+
+
+set(0,'DefaultAxesFontName','times');
+set(0,'DefaultAxesFontWeight','normal');
+set(0,'DefaultAxesFontSize',14);
+
+set(0,'DefaultAxesTickDir','in');
+set(0,'DefaultAxesXGrid','on');
+set(0,'DefaultAxesYGrid','on');
+set(0,'DefaultAxesZGrid','on');
+
+set(0,'DefaultTextFontName','times');
+set(0,'DefaultTextFontWeight','normal');
+set(0,'DefaultTextFontSize',16);
+
+set(0,'DefaultLineMarkersize',2);
+set(0,'DefaultLineLineWidth',.2);
+
+set(0,'DefaultSurfaceLineWidth',.65);
+
+clear all
+close all
+
+start_VOISE
+
