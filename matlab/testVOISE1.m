4781a4c85337683ee4ba7b3c3d7cf2791a68540b
patrick <patrick>
2009-11-10 17:14:05 +0000

    Updated to latest enhancements.

--- matlab/testVOISE1.m
+++ matlab/testVOISE1.m
@@ -3,9 +3,11 @@ function [params,IVD,DVD,MVD,CVD] = testVOISE1(nr,nc,ns,initSeeds,varargin)
 %
 % example:
 % [params,IVD,DVD,MVD,CVD] = testVOISE1(100,100,12,@randomSeeds)
+% [params,IVD,DVD,MVD,CVD] = testVOISE1(100,100,12,@randomSeeds,...
+%                            'dividePctile',90)
 
 %
-% $Id: testVOISE1.m,v 1.4 2009/11/06 15:43:15 patrick Exp $
+% $Id: testVOISE1.m,v 1.5 2009/11/10 17:14:05 patrick Exp $
 %
 % Copyright (c) 2008 
 % Patrick Guio <p.guio@ucl.ac.uk>
@@ -23,6 +25,10 @@ function [params,IVD,DVD,MVD,CVD] = testVOISE1(nr,nc,ns,initSeeds,varargin)
 % Public License for more details.
 %
 
+start_VOISE
+
+global voise
+
 % number of rows in image corresponds to y coordinate
 if ~exist('nr'),
   nr = 100;
@@ -38,6 +44,34 @@ if ~exist('ns'),
   ns = 40;
 end
 
+% load default parameters
+params = getDefaultVOISEParams;
+
+% VOISE algorithm parameters
+% Dividing
+params.dividePctile    = 80;
+params.d2Seeds         = 4;
+% Merging
+params.mergePctile     = 60;
+params.dmu             = 0.2;
+params.thresHoldLength = 0.3;
+% Regularise
+params.regMaxIter      = 1;
+
+% I/O parameters
+params.iFile           = [voise.root '/share/testPic.mat'];
+params.oDir            = [voise.root '/share/testPic/'];
+params.oMatFile        = 'voise';
+
+% diagnostics parameters
+params.divideExport    = false;
+params.mergeExport     = false;
+params.movDiag         = false;
+params.movPos          = [600 50 1300 1050]; % movie window size
+
+% parse online parameters
+params = parseArgs(params, varargin{:});
+
 x = linspace(-2,1,nc);
 y = linspace(-1,2,nr);
 
@@ -48,43 +82,30 @@ y = linspace(-1,2,nr);
 % [X(end,1)  , Y(end,1)  ] = -2   2
 % [X(end,end), Y(end,end)] =  1   2
 
-load ../share/testPic
-Z = double(Z);
-Z = max(Z(:))-Z;
-
-% load default parameters
-params = getDefaultVOISEParams;
+try
+  load(params.iFile)
+  Z = double(Z);
+  Z = max(Z(:))-Z;
+catch
+  error([params.iFile ' is not in your Matlab path\n' ...
+	      'Try to run start_VOISE']);
+end
 
 % set image, axes and related
-params.W = Z;
+params.W    = Z;
 params.Wlim = [min(params.W(:)) max(params.W(:))];
-params.x = [1:size(Z,1)];
+params.x    = [1:size(Z,1)];
 params.xlim = [min(params.x) max(params.x)];
-params.y = [1:size(Z,2)];
+params.y    = [1:size(Z,2)];
 params.ylim = [min(params.y) max(params.y)];
 
-params.dividePctile = 80;
-params.d2Seeds = 4;
-params.mergePctile = 60;
-params.dmu = 0.2;
-params.thresHoldLength = 0.3;
-params.regMaxIter = 1;
-
-params.oDir = '../share/';
-params.oMatFile = 'voise';
-params.divideExport = false;
-params.mergeExport = false;
-params.movDiag = false;
-params.movPos=[600 50 1300 1050]; % movie window size
+if ~exist(params.oDir,'dir')
+  unix(['mkdir -p ' params.oDir]);
+end
 
 % init seed of Mersenne-Twister RNG
 rand('twister',10);
 
-close all
-
-t = cputime;
+% run VOISE
 [params,IVD,DVD,MVD,CVD] = VOISE(params, ns, initSeeds, varargin{:});
-t = cputime-t;
-fprintf(1,'Elapsed time %2d:%2d:%2d\n', ...
-  floor(t/3600), floor(mod(t,3600)/60), floor(mod(mod(t,3660),60)));
 
