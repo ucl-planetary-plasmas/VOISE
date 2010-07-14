8ee632d23f7c90c20b08f96e40a589019ed2486d
patrick <patrick>
2010-07-14 15:46:32 +0000

    Initial version for figs 3.1 and 3.2 of Gander et al., 1994.

---
+++ matlab/limbFit/gander94figs23.m
@@ -0,0 +1,29 @@
+function gander94fig23
+% function gander94fig23
+%
+% figure 3.1 and 3.2 from Gander et al., 1994
+
+%
+% $Id: gander94figs23.m,v 1.1 2010/07/14 15:46:32 patrick Exp $
+%
+% Copyright (c) 2010
+% Patrick Guio <p.guio@ucl.ac.uk>
+%
+% All Rights Reserved.
+%
+% This program is free software; you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by the
+% Free Software Foundation; either version 2.  of the License, or (at your
+% option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+% Public License for more details.
+%
+
+% eq 3.6 from Gander et al., 1994
+x = [1, 2, 5, 7, 9, 6, 3, 8]';
+y = [7, 6, 8, 7, 5, 7, 2, 4]';
+
+
