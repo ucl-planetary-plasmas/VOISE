8a5eaded30d6fea9bbe1b88c0066b185a35d66de
patrick <patrick>
2010-09-04 06:57:13 +0000

    Added a function description. Added support to ignore lines starting wih #.

--- matlab/readVOISEconf.m
+++ matlab/readVOISEconf.m
@@ -1,7 +1,30 @@
 function params = readVOISEconf(conffile)
+% function params = readVOISEconf(conffile)
+%
+% Read a configuration file that contains
+% VOISE parameters and returns a complete
+% parameters structure. 
+% Note that parameters not specified in the
+% file are initialised to their default value.
+% The syntax is 'key' = 'value' on each line.
+% For example:
+% iNumSeeds = 12
+% RNGiseed = 10
+% dividePctile = 80
+% d2Seeds = 2
+% mergePctile = 50
+% dmu = 0.2
+% thresHoldLength = 0.3
+% regMaxIter = 2
+% iFile = ../share/input/sampleint.fits
+% oDir = ../share/output/sampleint/
+% oMatFile = voise
+% 
+% Lines starting with # are ignored.
+
 
 %
-% $Id: readVOISEconf.m,v 1.1 2010/09/03 17:12:33 patrick Exp $
+% $Id: readVOISEconf.m,v 1.2 2010/09/04 06:57:13 patrick Exp $
 %
 % Copyright (c) 2010
 % Patrick Guio <p.guio@ucl.ac.uk>
@@ -29,7 +52,7 @@ params = getDefaultVOISEParams();
 % The syntax is
 % field = value
 fid = fopen(conffile);
-C = textscan(fid, '%s=%s');
+C = textscan(fid, '%s=%s','CommentStyle','#');
 fclose(fid);
 key = C{1};
 val = C{2};
