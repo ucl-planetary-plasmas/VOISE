786faca06dc450b0eb45779d90e52f9581c8ef55
patrick <patrick>
2011-02-16 12:44:39 +0000

    Improved Compliance to Matlab exception. Updated code to support new parameters imageOrigo, pixelSize and pixelUnit.

--- matlab/loadImage.m
+++ matlab/loadImage.m
@@ -2,7 +2,7 @@ function params = loadImage(params)
 % function params = loadImage(params)
 
 %
-% $Id: loadImage.m,v 1.5 2010/09/06 18:59:05 patrick Exp $
+% $Id: loadImage.m,v 1.6 2011/02/16 12:44:39 patrick Exp $
 %
 % Copyright (c) 2010
 % Patrick Guio <p.guio@ucl.ac.uk>
@@ -20,55 +20,78 @@ function params = loadImage(params)
 % Public License for more details.
 %
 
-if strfind(params.iFile,'.mat'), % load mat-file
-  %   north_proj.mat is a mat file containing a polar projection of
-  %   Jupiter observed by HST:
-  %   Z           256x256         524288  double  image intensity
-  %   x             1x256           2048  double  x-axis (nun cols in image)
-  %   y           256x1             2048  double  y-axis (num rows in image)
-  try
+try
+
+  if strfind(params.iFile,'.mat'), % mat-file
+    %   north_proj.mat is a mat file containing a polar projection of
+    %   Jupiter observed by HST:
+    %   Z           256x256         524288  double  image intensity
+    %   x             1x256           2048  double  x-axis (# cols in Z)
+    %   y           256x1             2048  double  y-axis (# rows in Z)
+		if ~exist(params.iFile,'file'),
+		  ME = MException('MyFunction:verifyFile', ...
+                      ['Problem with file pointed to by ''params.iFile''' ...
+											'\nCheck the value for params.iFile=%s' ...
+                      '\nAnd/or try to run start_VOISE'],params.iFile);
+      throw(ME);
+		end
     im = load(params.iFile);
-  catch
-    error([params.iFile ' cannot be found or is not in the Matlab path']);
-  end
-  try
     % set image, axes and related
     params.W = im.Z;
-    params.x = im.x;
-    params.y = im.y;
-  catch
-    error([params.iFile ' does not have correct content' ...
-           sprintf('\nExpected content is Z(nr,nc), x(1,nc) and y(nr,1)')]);
-  end
+		if isfield(im,'x') & isfield(im,'y'),
+      params.x = im.x;
+      params.y = im.y;
+			% overwrite pixelSize and imageOrigo dedecude from x and y
+			params.pixelSize = [params.x(2)-params.x(1), params.y(2)-params.y(1)];
+			params.imageOrigo = [1-params.x(1)./params.pixelSize(1),...
+                           1-params.y(1)./params.pixelSize(2)];
+		else
+      params.x = ([1:size(params.W,2)]-params.imageOrigo(2))*params.pixelSize(2);
+      params.y = ([1:size(params.W,1)]-params.imageOrigo(1))*params.pixelSize(1);
+		end
+		if isfield(im,'pixelUnit') & ...
+       isa(im.pixelUnit,'cell') & ...
+       length(im.pixelUnit) == 2,
+			 params.pixelUnit = im.pixelUnit;
+    end
 
-  % set colour and axes limits
-  params.Wlim = [min(params.W(:)) max(params.W(:))];
-  params.xlim = [min(params.x) max(params.x)];
-  params.ylim = [min(params.y) max(params.y)];
+  elseif strfind(params.iFile,'.fits'), % fits-file
 
-elseif strfind(params.iFile,'.fits'), % load fits-file
-
-  try
+		if ~exist(params.iFile,'file')
+		  ME = MException('MyFunction:verifyFile', ...
+                      ['Problem with file pointed to by ''params.iFile''' ...
+											'\nCheck the value for params.iFile=%s' ...
+                      '\nAnd/or try to run start_VOISE'],params.iFile);
+      throw(ME);
+		end
     im = fitsread(params.iFile);
-  catch
-    error([params.iFile ' cannot be found']);
+    % set image, axes and related
+    params.W = im;
+    params.x = ([1:size(im,2)]-params.imageOrigo(2))*params.pixelSize(2);
+    params.y = ([1:size(im,1)]-params.imageOrigo(1))*params.pixelSize(1);
+
+  else, % neither mat-file nor fits-file
+
+    me = MException('MyFunction:fileTypeNotSupported',...
+                    '%s is not a fits- nor a mat-file',params.iFile);
+    throw(me);
+
   end
-  % set image, axes and related
-  params.W = im;
-  params.x = [1:size(im,2)];
-  params.y = [1:size(im,1)];
 
   % set colour and axes limits
-  params.Wlim = [min(params.W(:)) max(params.W(:))];
-  params.xlim = [min(params.x) max(params.x)];
-  params.ylim = [min(params.y) max(params.y)];
+  if isempty(params.Wlim),
+    params.Wlim = [min(params.W(:)) max(params.W(:))];
+  end
+  if isempty(params.xlim),
+    params.xlim = [min(params.x) max(params.x)];
+  end
+  if isempty(params.ylim),
+    params.ylim = [min(params.y) max(params.y)];
+  end
 
-else
+catch me
 
-  error([params.iFile ' is not recognised as a fits- or a mat-file']);
+  disp(['Problem when loading image file ' params.iFile '.']);
+  rethrow(me);
 
 end
-
-
-
-
