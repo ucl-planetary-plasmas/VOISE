a4296b94a01362c71156d9f954e3c02c4cdc7fde
patrick <patrick>
2011-02-25 12:06:57 +0000

    Utility function to extract values for a list of keywords in the Primary Data structure of a file in FITS format.

---
+++ matlab/getFitsPrimaryDataKeywordsValue.m
@@ -0,0 +1,52 @@
+function varargout = getFitsPrimaryDataKeywordsValue(filename,keywords)
+% function values = getFitsPrimaryDataKeywordsValue(filename,keywords)
+%
+% Return values of keywords contained in the Primary Data structure of a 
+% file in FITS format.
+% 
+% keywords is a cell of strings representing the keywords such as
+% {'PCX, 'PCY','CML','UDATE'}
+% 
+% values is a list of variable (one for each keyword). For the example
+% given above values should look like
+% [pcx,pcy,cml,udate]
+
+%
+% $Id: getFitsPrimaryDataKeywordsValue.m,v 1.1 2011/02/25 12:06:57 patrick Exp $
+%
+% Copyright (c) 2011
+% Patrick Guio <p.guio@ucl.ac.uk>
+%
+% All Rights Reserved.
+%
+% This program is free software; you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by the
+% Free Software Foundation; either version 2.  of the License, or (at your
+% option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+% Public License for more details.
+%
+
+info=fitsinfo(filename);
+
+values = cell(size(keywords));
+
+for k = 1:length(keywords),
+
+  keyword = keywords{k};
+  for i = 1:size(info.PrimaryData.Keywords,1),
+
+    if strcmp(info.PrimaryData.Keywords{i,1},keyword),
+
+		  values{k} = info.PrimaryData.Keywords{i,2};
+
+		end
+
+	end
+
+end
+
+varargout = {values{:}};
