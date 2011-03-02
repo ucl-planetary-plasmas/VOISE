d680b3d8f2e85728b82e762727799299fceee4c1
patrick <patrick>
2011-03-02 17:35:57 +0000

    Fixed the number of primary parameters for the legend.

--- matlab/limbFit/plotLimbFit.m
+++ matlab/limbFit/plotLimbFit.m
@@ -2,7 +2,7 @@ function plotLimbFit(params,fit1,fit2)
 % function plotLimbFit(params,fit1,fit2)
 
 %
-% $Id: plotLimbFit.m,v 1.3 2011/03/02 14:46:05 patrick Exp $
+% $Id: plotLimbFit.m,v 1.4 2011/03/02 17:35:57 patrick Exp $
 %
 % Copyright (c) 2009 
 % Patrick Guio <p.guio@ucl.ac.uk>
@@ -27,8 +27,9 @@ axis tight
 
 hold on
 
-[x0,y0] = disc(fit1.p0);
 pp = 1:length(fit1.p0);
+
+[x0,y0] = disc(fit1.p0);
 
 [x1,y1] = disc(fit1.p(pp));
 
@@ -40,21 +41,17 @@ legend('guess','fit1','fit2')
 if length(fit1.p0) == 3,
   [legh,objh,oh,om] = legend( ...
   sprintf('init C=(%.0f,%.0f) R=%.0f',fit1.p0), ...
-  sprintf('fit1 C=(%.0f\\pm%.1g,%.0f\\pm%.1g) R=%.0f\\pm%.1g',[fit1.p,fit1.psd]'), ...
-  sprintf('fit2 C=(%.0f\\pm%.1g,%.0f\\pm%.1g) R=%.0f\\pm%.1g',[fit2.p,fit2.psd]'));
+  sprintf('fit1 C=(%.0f\\pm%.1g,%.0f\\pm%.1g) R=%.0f\\pm%.1g',[fit1.p(pp),fit1.psd(pp)]'), ...
+  sprintf('fit2 C=(%.0f\\pm%.1g,%.0f\\pm%.1g) R=%.0f\\pm%.1g',[fit2.p(pp),fit2.psd(pp)]'));
 	set(objh(1),'fontsize',9);
 	%title(sprintf('C=(%.0f,%.0f) R=%.0f',fit2.p));
 elseif length(fit1.p0) == 5,
   [legh,objh,oh,om] = legend(...
   sprintf('init C=(%.0f,%.0f) a=%.0f b=%.0f \\theta=%.0f',fit1.p0), ...
-  sprintf('fit1 C=(%.0f\\pm%.1g,%.0f\\pm%.1g) a=%.0f\\pm%.1g b=%.0f\\pm%.1g \\theta=%.0f\\pm%.1g',[fit1.p,fit1.psd]'), ...
-  sprintf('fit2 C=(%.0f\\pm%.1g,%.0f\\pm%.1g) a=%.0f\\pm%.1g b=%.0f\\pm%.1g \\theta=%.0f\\pm%.1g',[fit2.p,fit2.psd]'));
+  sprintf('fit1 C=(%.0f\\pm%.1g,%.0f\\pm%.1g) a=%.0f\\pm%.1g b=%.0f\\pm%.1g \\theta=%.0f\\pm%.1g',[fit1.p(pp),fit1.psd(pp)]'), ...
+  sprintf('fit2 C=(%.0f\\pm%.1g,%.0f\\pm%.1g) a=%.0f\\pm%.1g b=%.0f\\pm%.1g \\theta=%.0f\\pm%.1g',[fit2.p(pp),fit2.psd(pp)]'));
 	set(objh(1),'fontsize',9);
 	%title(sprintf('C=(%.0f,%.0f) a=%.0f b=%.0f \\theta=%.0f',fit2.p));
 end
 
-
-
-
-
-
+hold off
