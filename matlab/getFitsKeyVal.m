f146c928a25c217392225467f16478bc6dd460c9
patrick <patrick>
2012-05-22 16:30:50 +0000

    Added extraction of instrument data of first image in addition to the primary data.

--- matlab/getFitsKeyVal.m
+++ matlab/getFitsKeyVal.m
@@ -1,8 +1,8 @@
 function varargout = getFitsKeywordsValue(filename,keywords)
 % function values = getFitsKeywordsValue(filename,keywords)
 %
-% Return values of keywords contained in the Primary Data structure of a 
-% file in FITS format.
+% Return values of keywords contained in the Primary Data structure and
+% the Instrument Data structure of the first image of a file in FITS format.
 % 
 % keywords is a cell of strings representing the keywords such as
 % {'PCX, 'PCY','CML','UDATE'}
@@ -12,7 +12,7 @@ function varargout = getFitsKeywordsValue(filename,keywords)
 % [pcx,pcy,cml,udate]
 
 %
-% $Id: getFitsKeyVal.m,v 1.1 2012/04/19 11:51:57 patrick Exp $
+% $Id: getFitsKeyVal.m,v 1.2 2012/05/22 16:30:50 patrick Exp $
 %
 % Copyright (c) 2011-2012 Patrick Guio <patrick.guio@gmail.com>
 % All Rights Reserved.
@@ -30,36 +30,45 @@ function varargout = getFitsKeywordsValue(filename,keywords)
 % You should have received a copy of the GNU General Public License
 % along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-info=fitsinfo(filename);
+info = fitsinfo(filename);
 
 values = cell(size(keywords));
 
 for k = 1:length(keywords),
 
   keyword = keywords{k};
-	if isfield(info,'PrimaryData') & isfield(info.PrimaryData,'Keywords')
-  for i = 1:size(info.PrimaryData.Keywords,1),
 
-    if strcmp(info.PrimaryData.Keywords{i,1},keyword),
+  if isfield(info,'PrimaryData') & isfield(info.PrimaryData,'Keywords')
+    for i = 1:size(info.PrimaryData.Keywords,1),
+      if strcmp(info.PrimaryData.Keywords{i,1},keyword),
+        values{k} = info.PrimaryData.Keywords{i,2};
+        fprintf(1,'PrimaryData %3d %8s - %s\n', i, ...
+                mydeblank(info.PrimaryData.Keywords{i,1}), ...
+                mydeblank(info.PrimaryData.Keywords{i,3}));
+      end
+    end
+  end
 
-		  values{k} = info.PrimaryData.Keywords{i,2};
-
-		end
-
-	end
-	end
-	if isfield(info,'Image') & isfield(info.Image,'Keywords')
-  for i = 1:size(info.Image(1).Keywords,1),
-
-    if strcmp(info.Image(1).Keywords{i,1},keyword),
-
-		  values{k} = info.Image(1).Keywords{i,2};
-
-		end
-
-	end
-	end
+  if isfield(info,'Image') & isfield(info.Image,'Keywords')
+		%for j = 1:length(info.Image),
+		for j = 1,
+      for i = 1:size(info.Image(j).Keywords,1),
+        if strcmp(info.Image(j).Keywords{i,1},keyword),
+          values{k} = info.Image(j).Keywords{i,2};
+          fprintf(1,'Image(%d)    %3d %8s - %s\n', j, i, ...
+                  mydeblank(info.Image(j).Keywords{i,1}), ...
+                  mydeblank(info.Image(j).Keywords{i,3}));
+        end
+      end
+    end
+  end
 
 end
 
 varargout = {values{:}};
+
+
+function string = mydeblank(string)
+
+string = deblank(fliplr(deblank(fliplr(string))));
+
