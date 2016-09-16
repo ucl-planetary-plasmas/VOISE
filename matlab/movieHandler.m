8c05778da8d75a27744380e3ed51811ed6a0dd36
patrick <patrick>
2016-09-16 15:57:26 +0000

    Added support for VideoWriter as avifile is deprecated.

--- matlab/movieHandler.m
+++ matlab/movieHandler.m
@@ -2,7 +2,7 @@ function movieHandler(params,action,varargin)
 % function movieHandler(params,action[,options])
 
 %
-% $Id: movieHandler.m,v 1.3 2012/04/16 16:54:27 patrick Exp $
+% $Id: movieHandler.m,v 1.4 2016/09/16 15:57:26 patrick Exp $
 %
 % Copyright (c) 2009-2012 Patrick Guio <patrick.guio@gmail.com>
 % All Rights Reserved.
@@ -20,6 +20,33 @@ function movieHandler(params,action,varargin)
 % You should have received a copy of the GNU General Public License
 % along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+% see http://stackoverflow.com/questions/11051307/approaches-to-create-a-video-in-matlab
+
+if exist('VideoWriter')==2,
+
+persistent myVideo
+
+switch lower(action)
+
+  case 'init',
+	  set(gcf,'position',params.movPos);
+		set(gcf,'DoubleBuffer','on');
+		set(gca,'nextplot','replacechildren', 'Visible','off');
+		myVideo = VideoWriter([params.oDir params.oMovFile]);
+		myVideo.Quality = 100;
+		myVideo.FrameRate = 2;
+		open(myVideo);
+	
+	case 'addframe',
+	  writeVideo(myVideo, getframe(gcf,[0 0 params.movPos(3:4)]));
+	
+	case 'close',
+		close(myVideo);
+
+end
+
+elseif exist('avifile')==2 && exist('addframe')==2, % deprecated
+
 persistent aviobj
 
 switch lower(action)
@@ -27,6 +54,7 @@ switch lower(action)
   case 'init',
 	  set(gcf,'position',params.movPos);
 		set(gcf,'DoubleBuffer','on');
+		set(gca,'nextplot','replacechildren', 'Visible','off');
 		aviobj = avifile([params.oDir params.oMovFile],'fps',2);
 	
 	case 'addframe',
@@ -34,5 +62,7 @@ switch lower(action)
 	
 	case 'close',
 	  aviobj = close(aviobj);
+
+end
 
 end
