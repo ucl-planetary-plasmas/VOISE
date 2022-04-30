53e1ed11300bd9fbe574e5c13dbbaf4cb958acc2
patrick <patrick>
2022-04-30 15:45:25 +0000

    Added node attributes pos with dim=3 and points=Sx,Sy,Smu and width with Sls.

--- matlab/VD2GraphViz.m
+++ matlab/VD2GraphViz.m
@@ -17,7 +17,7 @@ function varargout = VD2GraphViz(filename, VD, params)
 %     
 
 %
-% $Id: VD2GraphViz.m,v 1.2 2020/05/03 18:30:33 patrick Exp $
+% $Id: VD2GraphViz.m,v 1.3 2022/04/30 15:45:25 patrick Exp $
 %
 % Copyright (c) 2008-2012 Patrick Guio <patrick.guio@gmail.com>
 % All Rights Reserved.
@@ -39,14 +39,20 @@ function varargout = VD2GraphViz(filename, VD, params)
 % http://www.graphviz.org/content/attrs
 
 % Put the GraphViz file into the output directory
-filename = [params.oDir '/' filename];
+filename = [params.oDir filename];
+
+% Calculate equivalent scale length from VD as sqrt(S)
+[imls, Sls] = getVDOp(VD, params.W, 'sqrtLen');
+
+% Calculate median intensity from VD 
+[immu, Smu] = getVDOp(VD, params.W, 'median');
 
 % current time
 k = VD.k;
 
 s = sprintf('/*\n%s\n*/\n', ['sfdp ' ...
              '-Gsize="7,8" -Nshape=box -Efontsize=8 -Goverlap=prism ' ...
-						 'graph.gv -Tpdf > graph.pdf']);
+						 filename ' -Tpdf > graph.pdf']);
 
 % If the graph is strict then multiple edges are not allowed between the
 % same pairs of nodes
@@ -54,12 +60,20 @@ s = [s sprintf('\nstrict graph {\n')];
 
 %s = [s sprintf('%s\n%s\n%s\n', s2, s1, s2)];
 
-s1 = sprintf('\tgraph [splines=true overlap=false]');
+s1 = sprintf('\tgraph [splines=true overlap=false dim=3]');
 s2 = sprintf('\trankdir=LR; // Left to Right, instead of Top to Bottom');
 
 s = [s sprintf('%s\n%s\n', s1,s2)];
 
+% Attributes Graphviz documentation 
+% https://graphviz.org/doc/info/attrs.html
+
 %for i = VD.Sk', % for all seeds at current time 
+for i = 1:length(VD.Nk), % for all seeds that ever been registered
+  s = [s sprintf('\t%d ', i)];
+  s = [s sprintf('[pos="%.1f,%.1f,%.1f!" width=%.1f];\n',...
+                 VD.Sx(i),VD.Sy(i),Smu(i),Sls(i))];
+end
 for i = 1:length(VD.Nk), % for all seeds that ever been registered
   s = [s sprintf('\t%d -- ', i)];
 	sets = VD.Nk{i};
